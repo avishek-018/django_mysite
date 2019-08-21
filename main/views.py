6e79c19d44e370c96064d8a68de2481575b8a716
silenthunter007 <avishek.das.ayan@gmail.com>
2019-08-22 00:52:24 +0600

    Changes Committed

---
+++ main/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+
+# Create your views here.
+def homepage(request):
+	return render(
+			request = request,
+			template_name = 'main/home.html',
+			context = {"tutorials": Tutorial.objects.all}
+		)
+
+def register(request):
+	
+
+	if request.method == "POST":
+		form  = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			login(request, user)
+			return redirect('main:homepage')
+		else:
+			for msg in form.error_messages:
+				print(form.error_messages[msg])
+
+			return render(
+				request,
+				'main/register.html',
+				{'form': form}
+			)
+
+	form = UserCreationForm
+	return render(
+		request,
+		'main/register.html',
+		{'form': form}
+	)
